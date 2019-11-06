2f32ba5c357d9f459330e0dc8734ea713d7e63ae
Eric Loudon <loudon.eric@gmail.com>
2019-11-06 12:27:07 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,8 @@ class SongsController < ApplicationController
       @song = @artist.songs.find_by(id: params[:id])
       if @song.nil?
         redirect_to artist_songs_path(@artist), alert: "Song not found"
-    else
+      end
+      else
       @song = Song.find(params[:id])
     end
   end
